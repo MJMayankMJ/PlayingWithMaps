a48ce0908537fbe1a3d735972879dda57fff661b
MJMayankMJ <226301131@gkv.ac.in>
2024-12-25 17:35:19 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mayank Jangid on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Mayank Jangid on 12/25/24.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
